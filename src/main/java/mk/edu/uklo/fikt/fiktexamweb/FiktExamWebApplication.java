46bc223b6922ee6c5e7227d3311d4c16d3884be6
Davor <wizzi111@hotmail.com>
2019-03-01 13:57:34 +0100

    Added initial spring initializer project

---
+++ src/main/java/mk/edu/uklo/fikt/fiktexamweb/FiktExamWebApplication.java
@@ -0,0 +1,13 @@
+package mk.edu.uklo.fikt.fiktexamweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiktExamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiktExamWebApplication.class, args);
+	}
+
+}
