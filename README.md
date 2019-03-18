dcad4be71f34907975c371d56656fbdac47c7a59
Hristijan112 <hristijan0stojkovski@gmail.com>
2019-03-18 12:19:27 +0100

    Added User services

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fikt-exam-web
 Web application for practicing and making exams online.
+
+1.	Cloned the repository using git clone URL_
+2.	Merged the database branch into develop using git pull
+3.	Project opened
--- src/main/java/mk/edu/uklo/fikt/fiktexamweb/FiktExamWebApplication.java
+++ src/main/java/mk/edu/uklo/fikt/fiktexamweb/FiktExamWebApplication.java
@@ -2,8 +2,10 @@ package mk.edu.uklo.fikt.fiktexamweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FiktExamWebApplication {
 
 	public static void main(String[] args) {
